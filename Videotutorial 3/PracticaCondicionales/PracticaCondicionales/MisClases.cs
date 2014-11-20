b337a0f1f970077b6b9c34955cc649ffae7d07c7
zeroner <xeroner.z@gmail.com>
2014-11-19 21:46:39 -0600

    practicas condicionales y tipos de variables

---
+++ Videotutorial 3/PracticaCondicionales/PracticaCondicionales/MisClases.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MisClases 
+{
+    public class Leer
+    {
+        public static short datoShort()
+        {
+            try
+            {
+                return Int16.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                return Int16.MinValue;
+            }
+        }
+
+        public static int datosInt()
+        {
+            try
+            {
+                return Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                return Int32.MinValue;
+            }
+        }
+
+        public static long datosLong()
+        {
+            try
+            {
+                return Int64.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                return Int64.MinValue;
+            }
+        }
+
+        public static float datosFloat()
+        {
+            try
+            {
+                return Single.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                return Single.NaN;
+            }
+        }
+    }
+}
---
+++ Videotutorial 3/PracticaCondicionales/PracticaCondicionales/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MisClases;
+
+namespace PracticaCondicionales
+{
+    class CMenor
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c, menor;
+
+            Console.Write("a : ");
+            a = Leer.datosFloat();
+            Console.Write("b : ");
+            b = Leer.datosFloat();
+            Console.Write("c : ");
+            c = Leer.datosFloat();
+
+            if (a < b)
+                if (a < c)
+                    menor = a;
+                else
+                    menor = c;
+            else
+                if (b < c)
+                    menor = b;
+                else
+                    menor = c;
+
+            Console.WriteLine("Menor = " + menor);
+        }
+    }
+}
